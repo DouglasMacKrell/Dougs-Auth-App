05ad7a39e6a71bc8ef5e65de335eebee9bdb7507
DouglasMacKrell <douglasmackrell@pursuit.org>
2021-02-07 16:42:35 -0500

    Login init

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Dashboard() {
+export default function Login() {
     return (
         <div>
-            Dashboard
+            Login
         </div>
     )
 }
