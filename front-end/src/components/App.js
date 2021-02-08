7a8d15464827877572f563aacf8e3cb6999f466b
DouglasMacKrell <douglasmackrell@pursuit.org>
2021-02-07 22:43:37 -0500

    ForgotPassword init

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -9,20 +9,24 @@ import PrivateRoute from "./PrivateRoute"
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-      <div className="w-100" style={{ maxWidth: "400px"}}>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
         <Router>
           <AuthProvider>
             <Switch>
               <PrivateRoute exact path="/" component={Dashboard} />
               <Route path="/signup" component={Signup} />
               <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
             </Switch>
           </AuthProvider>
         </Router>
       </div>
     </Container>
-  )
+  );
 }
 
 export default App;
